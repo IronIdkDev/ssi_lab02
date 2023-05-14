0ec0b77fa9f2851d8c85a947c257979f1531f028
IronIdk <jmsfo@iscte-iul.pt>
2023-05-14 21:10:16 +0100

    Update alice.py

--- alice.py
+++ alice.py
@@ -1,7 +1,8 @@
-import os
 import time
 import cryptography
 import requests
+import logging
+import secrets
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -9,35 +10,45 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.exceptions import InvalidTag
 
+logging.basicConfig(level=logging.ERROR)
 
 # Function to establish connection and retrieve Bob's public key
 def get_bob_public_key():
-    while True:
+    max_retries = 3
+    retries = 0
+
+    while retries < max_retries:
         try:
             response = requests.get('http://localhost:8000/get_certificate', timeout=5)
             response.raise_for_status()
             return serialization.load_pem_public_key(response.content, backend=default_backend())
         except requests.exceptions.Timeout:
-            print("Connection timed out. Retrying...")
+            logging.error("Connection timed out. Retrying...")
             time.sleep(1)
         except requests.exceptions.ConnectionError:
-            print("Connection error. Retrying...")
+            logging.error("Connection error. Retrying...")
             time.sleep(1)
-        except requests.exceptions.RequestException as e:
-            print("Connection failed:", e)
-            print("Retrying...")
+        except requests.exceptions.RequestException:
+            logging.error("Connection failed:")
+            logging.error("Retrying...")
             time.sleep(1)
+
+        retries += 1
+
+    logging.error("Failed to establish a connection with Bob's public key.")
 
 # Function to encrypt the secret key using Bob's public key
 def encrypt_secret_key(secret_key, public_key):
-    random_key = os.urandom(32)
-    salt = os.urandom(16)
+    random_key = secrets.token_bytes(32)
+    salt = secrets.token_bytes(16)
     kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=salt, iterations=100000, backend=default_backend())
     key = kdf.derive(random_key)
     return public_key.encrypt(secret_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)), key
 
 # Function to decrypt the message using the secret key
 def decrypt_message(encrypted_message, key):
+    if len(encrypted_message) < 28:
+        raise ValueError("Invalid encrypted message")
     iv, ciphertext, tag = (
         encrypted_message[:12],
         encrypted_message[12:-16],
@@ -50,12 +61,12 @@ def decrypt_message(encrypted_message, key):
     )
     decryptor = cipher.decryptor()
     decryptor.authenticate_additional_data(b'')
-    decrypted_message = decryptor.update(ciphertext)
     try:
-        decryptor.finalize()
-    except cryptography.exceptions.InvalidTag as e:
-        raise e
-    return decrypted_message
+        decrypted_message = decryptor.update(ciphertext) + decryptor.finalize()
+        return decrypted_message
+    except InvalidTag:
+        raise InvalidTag("Authentication failed: Invalid tag")
+
 
 # Step 1: Alice gets Bob's public key
 bob_public_key = get_bob_public_key()
@@ -65,7 +76,11 @@ secret_key = b'This is a secret key'
 encrypted_secret_key, key = encrypt_secret_key(secret_key, bob_public_key)
 
 # Step 4: Alice sends the encrypted Secret Key to Bob
-response = requests.post('http://localhost:8000/receive_secret_key', data=encrypted_secret_key)
+try:
+    response = requests.post('http://localhost:8000/receive_secret_key', data=encrypted_secret_key)
+except requests.exceptions.ConnectionError as e:
+    print("Connection error occurred:", e)
+    exit(1)
 
 # Step 5: Alice receives the encrypted message from Bob
 encrypted_message = response.content
@@ -97,7 +112,7 @@ renewed_encrypted_message = response.content
 try:
     response = requests.get('http://localhost:8000/get_renewed_message', timeout=5)
     response.raise_for_status()
-    renewed_encrypted_message = response.content
+    renewed_encrypted_message_2 = response.content
 except requests.RequestException as e:
     print("Failed to retrieve renewed message:", e)
     exit(1)
