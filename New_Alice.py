66feb8aecb65fdbe40c06cecc1906f87ef8dd2d9
IronIdk <jmsfo@iscte-iul.pt>
2023-05-19 19:10:34 +0100

    private_key updated

--- New_Alice.py
+++ New_Alice.py
@@ -2,21 +2,19 @@ import socket
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric.padding import OAEP, MGF1
 
 import base64
 import os
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import padding
-from cryptography.hazmat.primitives.asymmetric.padding import OAEP, MGF1
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 # Server details
 SERVER_HOST = 'localhost'
 SERVER_PORT = 1234
-ENCRYPTION_ALGORITHIM = ''
 
 SECRET_KEY_MESSAGE = 'SECRET_KEY'
 GET_CERTIFICATE = 'GET_CERTIFICATE'
@@ -48,95 +46,85 @@ try:
     # Connect to the server
     client_socket.connect((SERVER_HOST, SERVER_PORT))
     print('Connected to the server.')
-    
+
     # Send GET_CERTIFICATE message to request a certificate
     client_socket.sendall(GET_CERTIFICATE.encode())
     print('#Step 1: Sent GET_CERTIFICATE request.')
 
-
     while True:
-        # Receive data from the client
-        data = client_socket.recv(1024)
+        # Receive data from the server
+        data = client_socket.recv(1024).decode()
 
         if not data:
             break
 
-        print('Received:', data.decode())
+        print('Received:', data)
 
-        if data.lower().decode() == 'quit' or data.lower().decode() == 'exit':
+        if data.lower() == 'quit' or data.lower() == 'exit':
             print('Restarting the server...')
             break
 
-        #if data == SENT_CERTIFICATE:
-        #data = client_socket.recv(1024).decode()
-        
-        print('#Step 2 (Alice part): Received SENT_CERTIFICATE:', data)
+        if data == SENT_CERTIFICATE:
+            # Read from bob.csr
+            with open("bob.csr", "rb") as f:
+                csr = f.read()
 
-        #read from bob.csr
-        with open("bob.csr", "rb") as f:
-            csr = f.read()
+            x509_csr = x509.load_pem_x509_csr(csr)
+            if x509_csr.is_signature_valid:
+                print("CSR signature is valid!!!")
+            else:
+                print("CSR signature is invalid!!!")
 
-        x509_csr = x509.load_pem_x509_csr(csr)
-        if x509_csr.is_signature_valid:
-            print("CSR signature is valid!!!")
-        else:
-            print("CSR signature is invalid!!!")
-            
-        
-        #get the public key from bob.csr
-        public_key = x509_csr.public_key()
+            # Get the public key from bob.csr
+            public_key = x509_csr.public_key()
 
-        # Generate a secret key
-        secret_key = b'ThisIsASecretKey'
-        print('#Step 4: Generated secret key successfully', '\n')
+            # Generate a secret key
+            secret_key = b'ThisIsASecretKey'
+            print('#Step 4: Generated secret key successfully', '\n')
 
-        # Encrypt the secret key using the certificate's public key
-        encrypted_key = public_key.encrypt(
-            secret_key,
-            OAEP(
-                mgf=MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
+            # Encrypt the secret key using the certificate's public key
+            encrypted_key = public_key.encrypt(
+                secret_key,
+                OAEP(
+                    mgf=MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
             )
-        )
-        print('#Step 5: Encrypted secret key successfully', '\n')
+            print('#Step 5: Encrypted secret key successfully', '\n')
 
-        # Send a message indicating we will send the parameters and the encrypted key
-        client_socket.send(SECRET_KEY_MESSAGE.encode())
-        print('#Step 6: Sent SECRET_KEY message to the server', '\n')
+            # Send a message indicating we will send the parameters and the encrypted key
+            client_socket.send(SECRET_KEY_MESSAGE.encode())
+            print('#Step 6: Sent SECRET_KEY message to the server', '\n')
 
-        # Send PARAMS message to send the details of the algorithms used to protect the messages.
-        # It may include information about the encryption algorithm, the hashing algorithm, the padding algorithm, etc.
-        PARAMS = ENCRYPTION_ALGORITHIM
-        client_socket.send(PARAMS.encode())
-        print('#Step 7: Sent PARAMS message to the server', '\n')
+            # Send PARAMS message to send the details of the algorithms used to protect the messages.
+            # It may include information about the encryption algorithm, the hashing algorithm, the padding algorithm, etc.
+            PARAMS = 'ENCRYPTION_ALGORITHM_DETAILS'
+            client_socket.send(PARAMS.encode())
+            print('#Step 7: Sent PARAMS message to the server', '\n')
 
-        # Convert the encrypted_key to bytes
-        encrypted_key_bytes = encrypted_key
+            # Convert the encrypted_key to bytes
+            encrypted_key_bytes = encrypted_key
 
-        # Send the length of the encrypted_key_bytes as a 4-byte integer
-        encrypted_key_length = len(encrypted_key_bytes)
-        client_socket.send(encrypted_key_length.to_bytes(4, byteorder='big'))
+            # Send the length of the encrypted_key_bytes as a 4-byte integer
+            encrypted_key_length = len(encrypted_key_bytes)
+            client_socket.send(encrypted_key_length.to_bytes(4, byteorder='big'))
 
-        # Send the encrypted_key_bytes
-        client_socket.send(encrypted_key_bytes)
+            # Send the encrypted_key_bytes
+            client_socket.send(encrypted_key_bytes)
 
-        # Send the encrypted key to the server
-        client_socket.sendall(encrypted_key)
-        print('#Step 8: Sent encrypted secret key to the server', '\n')
+            # Send the encrypted key to the server
+            client_socket.sendall(encrypted_key_bytes)
+            print('#Step 8: Sent encrypted secret key to the server', '\n')
 
+            # Receive the encrypted message from the server
+            encrypted_message = client_socket.recv(1024)
+            print('#Step 11: Received encrypted message from the server', '\n')
 
-        # Receive the encrypted message from the server
-        encrypted_message = client_socket.recv(1024)
-        print('#Step 11: Received encrypted message from the server', '\n')
-        decrypt_with_sk(encrypted_message, secret_key)
-        print('#Step 12: decrypted message from the server', '\n')
-
-
+            decrypted_message = decrypt_with_sk(encrypted_message, secret_key)
+            print('#Step 12: Decrypted message from the server:', decrypted_message.decode(), '\n')
 
     while True:
-        # Send data to the server
-
         secret_key = b'NEWSecretKey'
 
         encrypted_key = public_key.encrypt(
@@ -152,9 +140,6 @@ try:
         # Send a message indicating we will send the parameters and the encrypted key
         client_socket.sendall(RENEW_SECRET_KEY_MESSAGE.encode())
         print('#Step 14: Sent RENEW_SECRET_KEY message to the server', '\n')
-
-
-
 
         message = input('Enter a message (or "quit" to exit): ')
         client_socket.sendall(message.encode())
--- New_Bob.py
+++ New_Bob.py
@@ -4,33 +4,26 @@ from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
-
 import base64
-
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
 
 # Server details
 SERVER_HOST = 'localhost'
 SERVER_PORT = 1234
 SECRET_KEY_MESSAGE = 'SECRET_KEY'
-
 GET_CERTIFICATE = 'GET_CERTIFICATE'
 SENT_CERTIFICATE = 'SENT_CERTIFICATE'
 RENEW_SECRET_KEY_MESSAGE = 'RENEW_SECRET_KEY'
 
 
-#Creates a self Signed Certicifate
 def create_self_signed_certificate(key):
     print('Creating a self-signed certificate...')
-    print('To do this we need to collect some data. Please enter enter it below:')
-    country_name = 'PT' #input('Country Name (2 letter code): ')
-    state_or_province_name = 'PT' #input('State or Province Name (full name): ')
-    locality_name = 'PT' #input('Locality Name (eg, city): ')
-    organization_name = 'PT' #input('Organization Name (eg, company): ')
-    common_name = 'PT' #input('Common Name (e.g. server FQDN or YOUR name): ')
-    
+    print('To do this we need to collect some data. Please enter it below:')
+    country_name = 'PT'  # input('Country Name (2 letter code): ')
+    state_or_province_name = 'PT'  # input('State or Province Name (full name): ')
+    locality_name = 'PT'  # input('Locality Name (eg, city): ')
+    organization_name = 'PT'  # input('Organization Name (eg, company): ')
+    common_name = 'PT'  # input('Common Name (e.g. server FQDN or YOUR name): ')
+
     subject = issuer = x509.Name([
         x509.NameAttribute(NameOID.COUNTRY_NAME, country_name),
         x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, state_or_province_name),
@@ -38,63 +31,42 @@ def create_self_signed_certificate(key):
         x509.NameAttribute(NameOID.ORGANIZATION_NAME, organization_name),
         x509.NameAttribute(NameOID.COMMON_NAME, common_name),
     ])
-    
-    cert = x509.CertificateBuilder().subject_name(
-        subject
-    ).issuer_name(
-        issuer
-    ).public_key(
-        key.public_key()
-    ).serial_number(
-        x509.random_serial_number()
-    ).not_valid_before(
-        datetime.datetime.utcnow()
-    ).not_valid_after(
-        # Our certificate will be valid for 10 days
-        datetime.datetime.utcnow() + datetime.timedelta(days=10)
-    ).add_extension(
-        x509.BasicConstraints(ca=True, path_length=None),
-        critical=True,
-    ).add_extension(
-        x509.KeyUsage(digital_signature=True, key_encipherment=True, key_cert_sign=True, crl_sign=True, content_commitment=False, data_encipherment=False, key_agreement=False, encipher_only=False, decipher_only=False),
-        critical=True
-    # Sign our certificate with our private key
-    ).sign(key, hashes.SHA256())
-    # Write our certificate out to disk.
+
+    cert = x509.CertificateBuilder().subject_name(subject).issuer_name(issuer).public_key(
+        key.public_key()).serial_number(x509.random_serial_number()).not_valid_before(
+        datetime.datetime.utcnow()).not_valid_after(
+        datetime.datetime.utcnow() + datetime.timedelta(days=10)).add_extension(
+        x509.BasicConstraints(ca=True, path_length=None), critical=True).add_extension(
+        x509.KeyUsage(digital_signature=True, key_encipherment=True, key_cert_sign=True, crl_sign=True,
+                      content_commitment=False, data_encipherment=False, key_agreement=False, encipher_only=False,
+                      decipher_only=False), critical=True).sign(key, hashes.SHA256())
+
     with open("selfsigned_cert.pem", "wb") as f:
         f.write(cert.public_bytes(serialization.Encoding.PEM))
         print('Certificate created successfully.')
 
-
-    #CSR
     csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
-        # Details to be contained in the certificate
         x509.NameAttribute(NameOID.COUNTRY_NAME, country_name),
         x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, state_or_province_name),
         x509.NameAttribute(NameOID.LOCALITY_NAME, locality_name),
         x509.NameAttribute(NameOID.ORGANIZATION_NAME, organization_name),
         x509.NameAttribute(NameOID.COMMON_NAME, common_name),
-    ])).add_extension(
-        x509.SubjectAlternativeName([
-            # Alternative names for common name
-            x509.DNSName(u"mysite.com"),
-            x509.DNSName(u"www.mysite.com"),
-        ]),
-        critical=False,
-    # Sign the CSR with our private key
-    ).sign(key, hashes.SHA256())
+    ])).add_extension(x509.SubjectAlternativeName([
+        x509.DNSName(u"mysite.com"),
+        x509.DNSName(u"www.mysite.com"),
+    ]), critical=False).sign(key, hashes.SHA256())
 
-    # write the CSR to disk
     with open("bob.csr", "wb") as f:
         f.write(csr.public_bytes(serialization.Encoding.PEM))
-        
+
+
 def create_key_pair(filename):
     print('Creating a key pair...')
     key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=2048,
     )
-    # Write our key to disk for safe keeping
+
     with open(filename, "wb") as f:
         f.write(key.private_bytes(
             encoding=serialization.Encoding.PEM,
@@ -102,6 +74,7 @@ def create_key_pair(filename):
             encryption_algorithm=serialization.BestAvailableEncryption(b'bobP4ssword'),
         ))
         print('Key pair created successfully.')
+
 
 def read_key_pair(filename):
     password = (b'bobP4ssword')
@@ -112,52 +85,49 @@ def read_key_pair(filename):
         )
     print('Key pair read successfully.')
     return private_key
-    
+
+
 def send_certificate_to_client(client_socket):
     client_socket.sendall(SENT_CERTIFICATE.encode())
     print('Sent message: #Step 2: Sending the certificate.')
-    
-    #with open("selfsigned_cert.pem", "rb") as f:
-    #    certificate = f.read()
-    #client_socket.sendall(certificate)
-    print('Sent certificate to the client.')
 
-#def decrypt_secret_key(encrypted_key, params):
-#    print('\n', 'Decrypting the secret key...')
-#    # Decrypt the secret key
-#    decrypt_secret_key = rsa.decrypt(encrypted_key, params)
-#    return decrypt_secret_key
 
 def decipher_with_private_key(privkey, ciphertext, params):
     print("\nDeciphering with the private key...")
-    plaintext = privkey.decrypt(ciphertext, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
-    print("Plaintext = " + str(plaintext.decode()))
+    print("Ciphertext = " + str(ciphertext))
+    print("Ciphertext length = " + str(len(ciphertext)) + " bytes")
 
-# Encrypt with AES, using multiple modes
+    private_key_value = privkey.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+        encryption_algorithm=serialization.NoEncryption()
+    ).decode()
+    print("Private key value = " + private_key_value)
+    print("Private key length = " + str(len(private_key_value)) + " bytes")
+
+
 def do_aes(message, mode, key, iv=None, nonce=None):
     print("\nEncrypting with AES, 256-bit key, mode " + mode)
+    print("Data: " + str(message))
 
-    print("Data:" + str(message))
-
-    # AES works on blocks of 128bits (32 bytes) so we need to make user the message is multiple of the block lenght
     if len(message) % 16 != 0:
-        # handling the padding of the messages
-        padder = padding.PKCS7(algorithms.AES.block_size).padder()
+        padder = padding.PKCS7(hashes.AES.block_size).padder()
         paddeddata = padder.update(message)
         paddeddata += padder.finalize()
-        print("Data (padded):" +  str(paddeddata))
+        print("Data (padded):" + str(paddeddata))
         message = paddeddata
 
     print("KEY = " + str(base64.b64encode(key)))
+
     if mode == 'ECB':
         cipher = Cipher(algorithms.AES(key), modes.ECB())
-    if mode == 'CBC':
+    elif mode == 'CBC':
         cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
-    if mode == 'OFB':
+    elif mode == 'OFB':
         cipher = Cipher(algorithms.AES(key), modes.OFB(iv))
-    if mode == 'CFB':
+    elif mode == 'CFB':
         cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
-    if mode == 'CTR':
+    elif mode == 'CTR':
         cipher = Cipher(algorithms.AES(key), modes.CTR(nonce))
 
     encryptor = cipher.encryptor()
@@ -166,7 +136,7 @@ def do_aes(message, mode, key, iv=None, nonce=None):
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
     print("Ciphertext = " + str(base64.b64encode(ciphertext)))
 
-    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
+    unpadder = padding.PKCS7(hashes.AES.block_size).unpadder()
     data = unpadder.update(plaintext)
     plaintext_data = data + unpadder.finalize()
 
@@ -178,27 +148,19 @@ def start_server():
     create_key_pair('bob_key.pem')
     privkey = read_key_pair('bob_key.pem')
     create_self_signed_certificate(privkey)
-    while server_running == True:
+
+    while server_running:
         try:
-            # Create a socket object
             server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-            # Bind the server to the specified address and port
             server_socket.bind((SERVER_HOST, SERVER_PORT))
-
-            # Listen for incoming connections
             server_socket.listen()
-
             print('Waiting for incoming connections...')
 
-            # Accept a client connection
             client_socket, client_address = server_socket.accept()
             print('Connected to:', client_address)
 
             while True:
-                # Receive data from the client
                 data = client_socket.recv(1024).decode()
-
                 if not data:
                     break
 
@@ -209,59 +171,36 @@ def start_server():
                     break
 
                 if data == GET_CERTIFICATE:
-                    # Send the certificate to the client
                     send_certificate_to_client(client_socket)
                     print('#Step 2: Certificate sent to the client successfully.')
                     continue
 
                 if data == SECRET_KEY_MESSAGE or data == RENEW_SECRET_KEY_MESSAGE:
-                    # Prepare to receive PARAMS and encrypted secret key
                     params = client_socket.recv(1024)
                     print('Received PARAMS:', params)
-                    
+
                     encrypted_key = client_socket.recv(1024)
-                    #encrypted_key_length_bytes = client_socket.recv(4)
-                    #encrypted_key_length = int.from_bytes(encrypted_key_length_bytes, byteorder='big')
-                    #encrypted_key_bytes = client_socket.recv(encrypted_key_length)
-                    #encrypted_key = int.from_bytes(encrypted_key_bytes, byteorder='big')
                     print('Received encrypted secret key:', encrypted_key)
 
-                    # Decrypt the secret key
                     decrypted_secret_key = decipher_with_private_key(privkey, encrypted_key, params)
                     print('#Step 9: Decrypted secret key:', decrypted_secret_key)
 
-                    # Send a response to the client
                     response = 'Encryption completed.'
-
-                    #if params.toLower() == "aes":    
-                    #iv = os.urandom(16)
-                    #nonce = os.urandom(16)
-                    #do_aes(response, 'ECB', decrypted_secret_key, iv, nonce)
-                    #do_aes(response, 'CBC', decrypted_secret_key, iv, nonce)
-                    #do_aes(response, 'OFB', decrypted_secret_key, iv, nonce)
-                    #do_aes(response, 'CFB', decrypted_secret_key, iv, nonce)
-                    #do_aes(response, 'CTR', decrypted_secret_key, iv, nonce)
-                    print("#Step 10: mensage encrypted with SK")
+                    print("#Step 10: message encrypted with SK")
 
                     client_socket.sendall(response.encode())
                     continue
 
-
-                # Send a response to the client
-                #response = 'Message received: ' + data
-                #client_socket.sendall(response.encode())
-
-            # Close the client connection
             client_socket.close()
             print('Disconnected from:', client_address)
-
             server_socket.close()
 
             if data.lower() == 'quit' or data.lower() == 'exit':
                 continue
         except KeyboardInterrupt:
             print('Shutting down the server...')
-            server_running = False  
+            server_running = False
+
 
 # Start the server
 start_server()
